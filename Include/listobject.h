4f6ba52c5b31fe3ceeadf87305d5c01eeb8c94c7
zwb <1210611257@qq.com>
2021-11-25 10:00:07 +0800

    translate listobject.h

--- Include/listobject.h
+++ Include/listobject.h
@@ -6,11 +6,18 @@ Another generally useful object type is a list of object pointers.
 This is a mutable type: the list items can be changed, and items can be
 added or removed.  Out-of-range indices or non-list objects are ignored.
 
+另一种通常有用的对象类型是对象指针列表。
+这是一种可变类型：列表项可以更改，项可以添加或删除。将忽略超出范围的索引或非列表对象。
+
 *** WARNING *** PyList_SetItem does not increment the new item's reference
 count, but does decrement the reference count of the item it replaces,
 if not nil.  It does *decrement* the reference count if it is *not*
 inserted in the list.  Similarly, PyList_GetItem does not increment the
 returned item's reference count.
+
+PyList_SetItem不会增加新项目的引用计数，但会减少其替换项的引用计数（如果不是nil）
+如果它*未*插入列表，则它会*减少*引用计数。类似地，
+PyList_GetItem不会增加返回项目的引用计数。
 */
 
 #ifndef Py_LISTOBJECT_H
@@ -35,6 +42,13 @@ typedef struct {
      *
      * Items must normally not be NULL, except during construction when
      * the list is not yet visible outside the function that builds it.
+
+	   ob_item包含的是已经分配元素的空间,ob_size为当前元素数量
+	     0 <= ob_size <= allocated
+		 len(list) == ob_size
+		 ob_item == NULL implies ob_size == allocated == 0
+	   Items通常不为空。除非在构造期间，此时列表在构造它的函数之外还不可见。
+
      */
     Py_ssize_t allocated;
 } PyListObject;
